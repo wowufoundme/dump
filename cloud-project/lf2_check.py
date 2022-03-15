04cf1325ff643e5b6b41bcd64d9e1f053a16d696
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2022-03-15 14:36:36 -0400

    dump more files

---
+++ cloud-project/lf2_check.py
@@ -0,0 +1,212 @@
+data = {
+  "v1": [
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "_wC0Un8BewwEGnjElrnp",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "bVkjBJlAIwKAj9Aw1KVWjA"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "BgC0Un8BewwEGnjEmLox",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "nI1UYDCYUTt23TpGxqnLKg"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "CQC0Un8BewwEGnjEmLrp",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "0dDCDQz7DrKkSvu9h2hcQw"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "CgC0Un8BewwEGnjEmbqJ",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "ojbH3wnRu050hRhkmoxRiA"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "HAC0Un8BewwEGnjEnrpT",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "JaKLZrh1Y_2qB_0Gc3bk3A"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "HwC0Un8BewwEGnjEnrre",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "TzhAlljC_843JO7UDDUIaQ"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "IQC0Un8BewwEGnjEn7of",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "mUl0wtO7tPkBocuJqZ-7Dw"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "IwC0Un8BewwEGnjEn7qE",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "ZWP7yq9C2bhCcLkMR9BmVw"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "JwC0Un8BewwEGnjEoLo8",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "8ZBbl3VN-cu6PFOL0D7-8A"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "KAC0Un8BewwEGnjEoLpn",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "BPErPpKSNluwcjbKRgze3w"
+      }
+    }
+  ],
+  "v2": [
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "_wC0Un8BewwEGnjElrnp",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "bVkjBJlAIwKAj9Aw1KVWjA"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "BgC0Un8BewwEGnjEmLox",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "nI1UYDCYUTt23TpGxqnLKg"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "CQC0Un8BewwEGnjEmLrp",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "0dDCDQz7DrKkSvu9h2hcQw"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "CgC0Un8BewwEGnjEmbqJ",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "ojbH3wnRu050hRhkmoxRiA"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "HAC0Un8BewwEGnjEnrpT",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "JaKLZrh1Y_2qB_0Gc3bk3A"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "HwC0Un8BewwEGnjEnrre",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "TzhAlljC_843JO7UDDUIaQ"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "IQC0Un8BewwEGnjEn7of",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "mUl0wtO7tPkBocuJqZ-7Dw"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "IwC0Un8BewwEGnjEn7qE",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "ZWP7yq9C2bhCcLkMR9BmVw"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "JwC0Un8BewwEGnjEoLo8",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "8ZBbl3VN-cu6PFOL0D7-8A"
+      }
+    },
+    {
+      "_index": "yelp-restaurants",
+      "_type": "Restaurants",
+      "_id": "KAC0Un8BewwEGnjEoLpn",
+      "_score": 1.1700999,
+      "_source": {
+        "cuisine": "chinese",
+        "Business ID": "BPErPpKSNluwcjbKRgze3w"
+      }
+    }
+  ]
+}
+
+v1 = data["v1"]
+v2 = data["v2"]
+for i in range (0, 10):
+    if v1[i] == v2[i]:
+        print ("Matched...")
